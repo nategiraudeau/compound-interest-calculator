88f14e6b5289a02751691baf431d53c00dc3655c
nategiraudeau <natefg305@gmail.com>
2020-10-18 16:02:33 -0400

    Updates

---
+++ precache-manifest.fab2b69d4a82306d4b3b5c8cbdffb496.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09a3aed95bb04d20e38695aa1358123",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67113c8a04a2075d4b0f",
+    "url": "/static/css/2.6cda48cb.chunk.css"
+  },
+  {
+    "revision": "34adb811477155f10ccf",
+    "url": "/static/css/main.e8bd7aa0.chunk.css"
+  },
+  {
+    "revision": "67113c8a04a2075d4b0f",
+    "url": "/static/js/2.9f0fe5dc.chunk.js"
+  },
+  {
+    "revision": "8bb07fa084ecb6b917ec97e7f49757eb",
+    "url": "/static/js/2.9f0fe5dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34adb811477155f10ccf",
+    "url": "/static/js/main.a3cb2ab1.chunk.js"
+  },
+  {
+    "revision": "5216e62236d0c9842f59",
+    "url": "/static/js/runtime-main.e6bfb8cb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/compound-interest-calculator/precache-manifest.38e12d7c3b401e34d9258adacce794ba.js"
+  "/precache-manifest.fab2b69d4a82306d4b3b5c8cbdffb496.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/compound-interest-calculator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
